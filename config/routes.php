dd34a6a78964f89041b9fc0893fa3ac3d459ab6e
Marlin Cremers <m.cremers@cvo-technologies.com>
2015-06-06 23:33:40 +0200

    Add routes

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,10 @@
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 
+Router::plugin('Webshop/CustomerUsers', function (RouteBuilder $routeBuilder) {
+    $routeBuilder->fallbacks();
+});
+
 Router::prefix('panel', function (RouteBuilder $routeBuilder) {
     $routeBuilder->plugin('Webshop/CustomerUsers', function (RouteBuilder $routeBuilder) {
         $routeBuilder->fallbacks();
